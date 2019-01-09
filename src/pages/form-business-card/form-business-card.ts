9e1feec8bab9ce07b05e0c069b92244366542cf0
André Felicio <andree.felicio@gmail.com>
2019-01-09 10:06:49 -0200

    Criando a função com o action sheet para opções de seleção de foto. Colocando um placeholder para a foto de perfil.

--- src/pages/form-business-card/form-business-card.ts
+++ src/pages/form-business-card/form-business-card.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { NavController, NavParams, AlertController } from "ionic-angular";
+import {
+	NavController,
+	NavParams,
+	AlertController,
+	ActionSheetController
+} from "ionic-angular";
 
 @Component({
 	selector: "page-form-business-card",
@@ -11,10 +16,11 @@ export class FormBusinessCardPage {
 	signUpForm: FormGroup;
 
 	constructor(
+		private alertCtrl: AlertController,
+		public actionSheetCtrl: ActionSheetController,
 		public formBuilder: FormBuilder,
 		public navCtrl: NavController,
-		public navParams: NavParams,
-		private alertCtrl: AlertController
+		public navParams: NavParams
 	) {
 		//Variáveis de padrao regex para validações de telefone e cep
 		let phoneRegex = /^[\+]?[(]?[0-9]{2}[)]?[-\s\.]?[0-9]{4,5}[-\s\.]?[0-9]{4}$/im;
@@ -28,10 +34,22 @@ export class FormBusinessCardPage {
 			Endereco: ["", [Validators.required]],
 			EnderecoNumero: ["", [Validators.required]],
 			EnderecoComplemento: [""],
-			EnderecoCep: ["", Validators.compose([Validators.required, Validators.pattern(cepRegex)])],
+			EnderecoCep: [
+				"",
+				Validators.compose([
+					Validators.required,
+					Validators.pattern(cepRegex)
+				])
+			],
 			EnderecoCidade: ["", [Validators.required]],
 			EnderecoUf: ["", [Validators.required, Validators.maxLength(2)]],
-			TelefoneComercial: ["", Validators.compose([Validators.required, Validators.pattern(phoneRegex)])],
+			TelefoneComercial: [
+				"",
+				Validators.compose([
+					Validators.required,
+					Validators.pattern(phoneRegex)
+				])
+			],
 			TelefoneCelular: ["", Validators.pattern(phoneRegex)],
 			Email: [
 				"",
@@ -46,13 +64,79 @@ export class FormBusinessCardPage {
 
 		//Variável com configuração do alert exibido
 		let alert = this.alertCtrl.create({
-			title: 'Cartão gerado com sucesso!',
-			subTitle: 'O cartão será enviado para o seu e-mail.',
-			buttons: ['Ok!'],
+			title: "Cartão gerado com sucesso!",
+			subTitle: "O cartão será enviado para o seu e-mail.",
+			buttons: ["Ok!"],
 			enableBackdropDismiss: true
-		  });
-		  //Apresentação do alert
-		  alert.present();
+		});
+		//Apresentação do alert
+		alert.present();
+	}
 
+	//Função para exibir opções de upload de foto de perfil via action sheet
+	onUploadPhoto(): void {
+		console.log("start actionsheet");
+
+		let showExcluir: boolean = this.signUpForm.controls['FotoPerfil'].value;
+
+		//variável com configuração do action sheet
+		let actionSheet = this.actionSheetCtrl.create({
+			title: "Alterar foto de perfil",
+			buttons: [
+			  {
+				text: 'TIRAR FOTO',
+				icon: "camera",
+				handler: () => {
+				  console.log('TIRAR clicked');
+				}
+			  },
+			  {
+				text: 'GALERIA',
+				icon: "image",
+				handler: () => {
+				  console.log('GALERIA clicked');
+				}
+			  }
+			]
+		});
+
+
+		if(showExcluir){
+			actionSheet.addButton({
+				text: 'EXCLUIR FOTO',
+				icon: "trash",
+				role: 'destructive',
+				handler: () => {
+				  console.log('EXCLUIR clicked');
+				}
+			});
+		}
+
+
+
+		actionSheet.addButton({
+			text: "CANCELAR",
+			icon: "close",
+			role: "cancel"
+		});
+
+		actionSheet.present();
+	}
+
+	createButtons() {
+		let buttons = [];
+
+		/*for (var index in this.possibleButtons) {
+		  let button = {
+			text: this.possibleButtons[index].text,
+			icon: this.possibleButtons[index].icon,
+			handler: () => {
+			  console.log('setting icon ' + this.possibleButtons[index].icon);
+			  return true;
+			}
+		  }
+		  buttons.push(button);
+		}*/
+		return buttons;
 	}
 }
